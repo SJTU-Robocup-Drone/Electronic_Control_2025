653c5b58661b7c0f19794c547142006b5d59001a
lichangxian1 <3280519559@qq.com>
2025-08-28 21:44:55 +0800

    refactor:Make the board_ws able to pass the catkin_make

---
+++ board_ws/build/board_ros/CMakeFiles/set_param.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/li/Drone/board_ws/devel/lib/board_ros/set_param"
+  "/home/li/Drone/board_ws/devel/lib/board_ros/set_param.pdb"
+  "CMakeFiles/set_param.dir/src/set_param.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/set_param.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ board_ws/build/board_ros/CMakeFiles/waypoint_tracking.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/li/Drone/board_ws/devel/lib/board_ros/waypoint_tracking"
+  "/home/li/Drone/board_ws/devel/lib/board_ros/waypoint_tracking.pdb"
+  "CMakeFiles/waypoint_tracking.dir/src/waypoint_tracking.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/waypoint_tracking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
